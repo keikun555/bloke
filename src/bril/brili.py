3b1702e04096a9b2cf7002565432959821c46796
Kei Imada <>
2023-10-31 13:48:43 -0400

    brili.py can now handle python primitive types

--- src/bril/brili.py
+++ src/bril/brili.py
@@ -3,19 +3,21 @@ import json
 import re
 import subprocess
 import sys
-from typing import NamedTuple
+from typing import NamedTuple, Iterable
 
 import click
 
-from bril.typing_bril import Program
+from bril.typing_bril import PrimitiveType, Program
 
 BRILI_COMMAND = "brili"
 BRILI_PROFILE_FLAG = "-p"
 BRILI_PROFILE_REGEX = re.compile(r"^total_dyn_inst: (\d+)$")
 BRILI_ERROR_REGEX = re.compile(r"^error: (.*)$")
 
+
 class BriliOutput(NamedTuple):
     """What brili would output"""
+
     returncode: int
     stdout: str
     stderr: str
@@ -23,14 +25,34 @@ class BriliOutput(NamedTuple):
     total_dyn_inst: int | None
 
 
-def brili(program: Program, arguments: list[str], profile: bool = False) -> BriliOutput:
-    """Interface to brili"""
+def python_to_brili_argument(argument: PrimitiveType) -> str:
+    """Convert Python's Bril primitive type to brili argument"""
+    if isinstance(argument, (int, float)):
+        return str(argument)
+    if isinstance(argument, bool):
+        return str(argument).lower()
+
+    raise ValueError(f"python_to_brili_argument, unsupported: {argument}")
+
+
+def brili(
+    program: Program, arguments: Iterable[PrimitiveType], profile: bool = False
+) -> BriliOutput:
+    """Interface to brili, supports Python's Bril primitive type"""
+    string_arguments = (python_to_brili_argument(arg) for arg in arguments)
+    return brili_string(program, string_arguments, profile=profile)
+
+
+def brili_string(
+    program: Program, arguments: Iterable[str], profile: bool = False
+) -> BriliOutput:
+    """Interface to brili, takes in raw strings for arguments"""
     command = [BRILI_COMMAND]
     if profile:
         command.append(BRILI_PROFILE_FLAG)
 
     command.extend(arguments)
-    result = subprocess.run( # pylint: disable=subprocess-run-check
+    result = subprocess.run(  # pylint: disable=subprocess-run-check
         command, input=json.dumps(program).encode("utf-8"), capture_output=True
     )
 
@@ -70,7 +92,7 @@ def brili(program: Program, arguments: list[str], profile: bool = False) -> Bril
 @click.argument("arguments", nargs=-1)
 def main(profile: bool, arguments: list[str]):
     program: Program = json.load(sys.stdin)
-    print(brili(program, arguments, profile=profile))
+    print(brili_string(program, arguments, profile=profile))
 
 
 if __name__ == "__main__":
