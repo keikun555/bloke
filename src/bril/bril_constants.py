fdc44e709db479a05b04665cf5307be7517d5a4b
Kei Imada <>
2023-11-01 15:05:04 -0400

    Add operation argument and return types

--- src/bril/bril_constants.py
+++ src/bril/bril_constants.py
@@ -1,8 +1,9 @@
 """
 Constants used in Bril tools
 """
+from typing import Literal
 
-from bril.typing_bril import Operation
+from bril.typing_bril import BrilType, Operation
 
 ENTRY_FUNCTION_NAME = "main"
 
@@ -19,3 +20,61 @@ COMMUTATIVE_OPERATIONS: list[Operation] = [
     "feq",
     "ceq",
 ]
+
+GenericType = Literal["generic"]
+
+OPERATORS: tuple[
+    tuple[Operation, tuple[BrilType | GenericType, ...], BrilType | GenericType | None]
+] = (
+    ("const", ("generic"), "generic"),
+    # core
+    ("add", ("int", "int"), "int"),
+    ("mul", ("int", "int"), "int"),
+    ("sub", ("int", "int"), "int"),
+    ("div", ("int", "int"), "int"),
+    ("eq", ("int", "int"), "int"),
+    ("lt", ("int", "int"), "bool"),
+    ("gt", ("int", "int"), "bool"),
+    ("le", ("int", "int"), "bool"),
+    ("ge", ("int", "int"), "bool"),
+    ("not", ("bool"), "bool"),
+    ("and", ("bool", "bool"), "bool"),
+    ("or", ("bool", "bool"), "bool"),
+    ("jmp", (), None),
+    ("br", ("bool"), None),
+    ("call", (), None),  # TODO Need to generalize this
+    ("ret", ("generic"), None),
+    ("id", ("generic"), "generic"),
+    ("print", ("generic"), None),  # TODO Need to do variadic
+    ("nop", (), None),
+    # memory
+    ("free", ({"ptr": "generic"}), None),
+    ("alloc", ("int"), {"ptr": "generic"}),
+    ("store", ({"ptr": "generic"}, "generic"), None),
+    ("load", ({"ptr": "generic"}), "generic"),
+    ("ptradd", ({"ptr": "generic"}, "int"), {"ptr": "generic"}),
+    # float
+    ("fadd", ("float", "float"), "float"),
+    ("fmul", ("float", "float"), "float"),
+    ("fsub", ("float", "float"), "float"),
+    ("fdiv", ("float", "float"), "float"),
+    ("feq", ("float", "float"), "float"),
+    ("flt", ("float", "float"), "bool"),
+    ("fle", ("float", "float"), "bool"),
+    ("fgt", ("float", "float"), "bool"),
+    ("fge", ("float", "float"), "bool"),
+    # SSA
+    ("phi", ("generic"), "generic"),  # TODO Need to do variadic
+    # speculative execution
+    ("speculate", (), None),
+    ("commit", (), None),
+    ("guard", (), None),
+    # character
+    ("ceq", ("char", "char"), "bool"),
+    ("clt", ("char", "char"), "bool"),
+    ("cle", ("char", "char"), "bool"),
+    ("cgt", ("char", "char"), "bool"),
+    ("cge", ("char", "char"), "bool"),
+    ("char2int", ("char"), "int"),
+    ("int2char", ("int"), "char"),
+)
